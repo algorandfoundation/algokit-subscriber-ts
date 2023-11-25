11a4636a64df345d64a46ebc90cd7317cf257bce
Rob Moore (MakerX) <rob.moore@makerx.com.au>
2023-11-25 17:05:48 +0800

    chore: Fixed test setup

---
+++ jest.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from '@jest/types'
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['src/**/*.spec.ts'],
+  testTimeout: 10000,
+  setupFiles: ['<rootDir>/tests/setup.ts'],
+  moduleDirectories: ['node_modules', 'src'],
+  transform: {
+    '<regex_match_files>': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.test.json',
+      },
+    ],
+  },
+  coveragePathIgnorePatterns: ['tests'],
+}
+export default config
