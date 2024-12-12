96239f27c28d74379098247c60f524d39d43fb3a
Hoang Dinh <d.conghoang@gmail.com>
2024-12-12 13:45:09 +1000

    fix tests

--- tests/subscribed-transactions.ts
+++ tests/subscribed-transactions.ts
@@ -15,17 +15,19 @@ function getReceiver(transaction: SubscribedTransaction): string | undefined {
 export function getSubscribedTransactionForDiff(transaction: SubscribedTransaction): any {
   const t = {
     ...transaction,
-    applicationTransaction: {
-      ...transaction.applicationTransaction,
-      accounts: transaction.applicationTransaction?.accounts?.map((a) => a.toString()),
-      applicationArgs: transaction.applicationTransaction?.applicationArgs?.map((a) => Buffer.from(a).toString('base64')),
-      approvalProgram: transaction.applicationTransaction?.approvalProgram
-        ? Buffer.from(transaction.applicationTransaction.approvalProgram).toString('base64')
-        : undefined,
-      clearStateProgram: transaction.applicationTransaction?.clearStateProgram
-        ? Buffer.from(transaction.applicationTransaction.clearStateProgram).toString('base64')
-        : undefined,
-    },
+    applicationTransaction: transaction.applicationTransaction
+      ? {
+          ...transaction.applicationTransaction,
+          accounts: transaction.applicationTransaction?.accounts?.map((a) => a.toString()),
+          applicationArgs: transaction.applicationTransaction?.applicationArgs?.map((a) => Buffer.from(a).toString('base64')),
+          approvalProgram: transaction.applicationTransaction?.approvalProgram
+            ? Buffer.from(transaction.applicationTransaction.approvalProgram).toString('base64')
+            : undefined,
+          clearStateProgram: transaction.applicationTransaction?.clearStateProgram
+            ? Buffer.from(transaction.applicationTransaction.clearStateProgram).toString('base64')
+            : undefined,
+        }
+      : undefined,
     reKeyTo: transaction.rekeyTo ? transaction.rekeyTo.toString() : undefined,
     genesisHash: transaction.genesisHash ? Buffer.from(transaction.genesisHash).toString('base64') : '',
     group: transaction.group ? Buffer.from(transaction.group).toString('base64') : undefined,
@@ -33,6 +35,25 @@ export function getSubscribedTransactionForDiff(transaction: SubscribedTransacti
     note: transaction.note ? Buffer.from(transaction.note).toString('base64') : undefined,
     logs: transaction.logs?.map((l) => Buffer.from(l).toString('base64')),
     innerTxns: transaction.innerTxns?.map((i) => getSubscribedTransactionForDiff(i)),
+    keyregTransaction: transaction.keyregTransaction
+      ? {
+          ...transaction.keyregTransaction,
+          selectionParticipationKey: transaction.keyregTransaction.selectionParticipationKey
+            ? Buffer.from(transaction.keyregTransaction.selectionParticipationKey).toString('base64')
+            : undefined,
+          stateProofKey: transaction.keyregTransaction.stateProofKey
+            ? Buffer.from(transaction.keyregTransaction.stateProofKey).toString('base64')
+            : undefined,
+          voteParticipationKey: transaction.keyregTransaction.voteParticipationKey
+            ? Buffer.from(transaction.keyregTransaction.voteParticipationKey).toString('base64')
+            : undefined,
+        }
+      : undefined,
+    signature: transaction.signature
+      ? {
+          sig: transaction.signature.sig ? Buffer.from(transaction.signature.sig).toString('base64') : undefined,
+        }
+      : undefined,
   }
 
   return structuredClone(clearUndefineds(t as any))
