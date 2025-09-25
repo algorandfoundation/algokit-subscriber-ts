d09e557189e184eedb55fc9d709c7da895bb3d11
Neil Campbell <neil.campbell@makerx.com.au>
2025-09-26 01:42:14 +0800

    feat: map rejectVersion on the transaction

--- tests/scenarios/transform-appl-txn.spec.ts
+++ tests/scenarios/transform-appl-txn.spec.ts
@@ -340,4 +340,139 @@ describe('Application transaction', () => {
       ]
     `)
   })
+
+  it('correctly handles application reject version', async () => {
+    const block = new algosdk.modelsv2.BlockResponse({
+      block: new algosdk.Block({
+        header: new algosdk.BlockHeader({
+          round: 23056n,
+          timestamp: 1758786416n,
+          genesisID: 'dockernet-v1',
+          genesisHash: new Uint8Array([
+            224, 98, 0, 143, 179, 147, 51, 66, 97, 55, 93, 12, 84, 251, 18, 30, 102, 58, 226, 21, 145, 85, 241, 231, 59, 55, 213, 85, 167,
+            79, 239, 157,
+          ]),
+          branch: new Uint8Array([
+            13, 50, 44, 85, 195, 58, 235, 18, 240, 186, 208, 118, 213, 194, 136, 113, 62, 70, 155, 114, 31, 20, 190, 74, 219, 166, 55, 165,
+            198, 239, 63, 184,
+          ]),
+          seed: new Uint8Array([
+            13, 50, 44, 85, 195, 58, 235, 18, 240, 186, 208, 118, 213, 194, 136, 113, 62, 70, 155, 114, 31, 20, 190, 74, 219, 166, 55, 165,
+            198, 239, 63, 184,
+          ]),
+          proposer: algosdk.Address.zeroAddress(),
+          proposerPayout: 0n,
+          feesCollected: 1000n,
+          bonus: 10000000n,
+          rewardState: new algosdk.RewardState({
+            feeSink: new algosdk.Address(
+              new Uint8Array([
+                7, 218, 203, 75, 109, 158, 209, 65, 177, 117, 118, 189, 69, 154, 230, 66, 29, 72, 109, 163, 212, 239, 34, 71, 196, 9, 163,
+                150, 184, 46, 162, 33,
+              ]),
+            ),
+            rewardsPool: new algosdk.Address(
+              new Uint8Array([
+                255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+                255, 255, 255, 255, 255, 255, 255,
+              ]),
+            ),
+            rewardsLevel: 0n,
+            rewardsRate: 0n,
+            rewardsResidue: 0n,
+            rewardsRecalculationRound: 500000n,
+          }),
+          upgradeState: new algosdk.UpgradeState({
+            currentProtocol: 'https://github.com/algorandfoundation/specs/tree/953304de35264fc3ef91bcd05c123242015eeaed',
+            nextProtocol: '',
+            nextProtocolApprovals: 0n,
+            nextProtocolSwitchOn: 0n,
+            nextProtocolVoteBefore: 0n,
+          }),
+          txnCommitments: new algosdk.TxnCommitments({
+            nativeSha512_256Commitment: new Uint8Array([
+              137, 51, 213, 64, 101, 123, 159, 152, 130, 101, 105, 49, 148, 109, 69, 38, 48, 61, 102, 185, 119, 16, 21, 160, 62, 254, 89,
+              54, 147, 175, 138, 29,
+            ]),
+            sha256Commitment: new Uint8Array([
+              88, 232, 193, 250, 126, 51, 202, 241, 244, 41, 212, 183, 227, 211, 58, 245, 237, 172, 178, 197, 254, 95, 75, 42, 129, 75, 135,
+              206, 74, 92, 211, 58,
+            ]),
+          }),
+          stateproofTracking: new Map([
+            [
+              0,
+              new algosdk.StateProofTrackingData({
+                stateProofVotersCommitment: new Uint8Array(),
+                stateProofOnlineTotalWeight: 0n,
+                stateProofNextRound: 23296n,
+              }),
+            ],
+          ]),
+          participationUpdates: new algosdk.ParticipationUpdates({
+            expiredParticipationAccounts: [],
+            absentParticipationAccounts: [],
+          }),
+          upgradeVote: new algosdk.UpgradeVote({
+            upgradePropose: '',
+            upgradeDelay: 0n,
+            upgradeApprove: false,
+          }),
+          txnCounter: 28645n,
+        }),
+        payset: [
+          new algosdk.SignedTxnInBlock({
+            signedTxn: new algosdk.SignedTxnWithAD({
+              signedTxn: new algosdk.SignedTransaction({
+                sig: new Uint8Array([
+                  136, 247, 170, 92, 235, 121, 107, 200, 114, 94, 68, 237, 20, 28, 22, 135, 85, 121, 147, 120, 184, 185, 57, 205, 255, 58,
+                  27, 115, 169, 216, 89, 108, 232, 145, 194, 90, 51, 146, 211, 173, 3, 173, 110, 176, 236, 224, 152, 209, 106, 43, 115, 140,
+                  193, 27, 56, 45, 53, 47, 67, 172, 186, 13, 199, 5,
+                ]),
+                txn: new algosdk.Transaction({
+                  type: algosdk.TransactionType.appl,
+                  sender: new algosdk.Address(
+                    new Uint8Array([
+                      15, 186, 47, 211, 220, 116, 124, 144, 198, 46, 85, 43, 199, 18, 249, 184, 228, 178, 228, 81, 2, 57, 11, 216, 17, 182,
+                      204, 34, 254, 97, 100, 250,
+                    ]),
+                  ),
+                  suggestedParams: {
+                    fee: 0,
+                    minFee: 1000,
+                    firstValid: 23055,
+                    lastValid: 24055,
+                  },
+                  appCallParams: {
+                    appIndex: 28639n,
+                    onComplete: algosdk.OnApplicationComplete.NoOpOC,
+                    appArgs: [new Uint8Array([241, 126, 128, 165]), new Uint8Array([0, 5, 104, 101, 108, 108, 111])],
+                    access: [],
+                    rejectVersion: 3,
+                  },
+                }),
+              }),
+              applyData: new algosdk.ApplyData({
+                applicationID: undefined,
+                evalDelta: new algosdk.EvalDelta({
+                  globalDelta: new Map(),
+                  localDeltas: new Map(),
+                  logs: [new Uint8Array([21, 31, 124, 117, 0, 12, 72, 101, 108, 108, 111, 44, 32, 104, 101, 108, 108, 111])],
+                  innerTxns: [],
+                }),
+              }),
+            }),
+            hasGenesisID: true,
+            hasGenesisHash: false,
+          }),
+        ],
+      }),
+      cert: new UntypedValue(new Map([['rnd', 23056n]])),
+    })
+
+    const txn = getBlockTransactions(block)[0]
+    const transaction = getIndexerTransactionFromAlgodTransaction(txn)
+
+    expect(transaction.applicationTransaction?.rejectVersion).toBe(3)
+  })
 })
